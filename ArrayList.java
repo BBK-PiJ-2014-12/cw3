118410f105b895c3806022969f5c718ddbbdc949
BBK-PiJ-2014-12 <zsolt.balvanyos@gmail.com>
2015-01-17 12:35:27 +0000

    ArrayList finished

--- ArrayList.java
+++ ArrayList.java
@@ -3,10 +3,9 @@ public class ArrayList implements List{
 	private static final int EMPTY_STRUCTURE = 1;
 	private static final int INDEX_OUT_OF_BOUNDS = 2;
 	private static final int INVALID_ARGUMENT = 3;
-	private int STORAGE = 3;
-	private int size = 0;
+	private int Storage = 3;
 	private int lastElement = 0;
-	private ReturnObjectImpl[] arrayList = new ReturnObjectImpl[STORAGE];
+	private ReturnObjectImpl[] arrayList = new ReturnObjectImpl[Storage];
 	
 	ErrorObject errObj = new ErrorObject();
 	
@@ -21,21 +20,13 @@ public class ArrayList implements List{
 			return false;
 		}
 	}
-
-	public int size() {
+	public int getStorage() {
 		return arrayList.length;
 	}
+	public int size() {
+		return lastElement + 1;
+	}
 
-	/**
-	 * Returns the elements at the given position. 
-	 * 
-	 * If the index is negative or greater or equal than the size of
-	 * the list, then an appropriate error must be returned.
-	 * 
-	 * @param index the position in the list of the item to be retrieved
-	 * @return the element or an appropriate error message, 
-	 *         encapsulated in a ReturnObject
-	 */
 	public ReturnObject get(int index) {
 		if(isEmpty()) {
 			return errObj.createErrorObject(EMPTY_STRUCTURE);
@@ -46,74 +37,84 @@ public class ArrayList implements List{
 		}
 	}
 
-	/**
-	 * Returns the elements at the given position and removes it
-	 * from the list. The indeces of elements after the removed
-	 * element must be updated accordignly.
-	 * 
-	 * If the index is negative or greater or equal than the size of
-	 * the list, then an appropriate error must be returned.
-	 * 
-	 * @param index the position in the list of the item to be retrieved
-	 * @return the element or an appropriate error message, 
-	 *         encapsulated in a ReturnObject
-	 */
-	public ReturnObject remove(int index);
-
-	/**
-	 * Adds an element to the list, inserting it at the given
-	 * position. The indeces of elements at and after that position
-	 * must be updated accordignly.
-	 * 
-	 * If the index is negative or greater or equal than the size of
-	 * the list, then an appropriate error must be returned.
-	 * 
-	 * If a null object is provided to insert in the list, the
-	 * request must be ignored and an appropriate error must be
-	 * returned.
-	 * 
-	 * @param index the position at which the item should be inserted in
-	 *              the list
-	 * @param item the value to insert into the list
-	 * @return an ReturnObject, empty if the operation is successful
-	 *         the item added or containing an appropriate error message
-	 */
-	public ReturnObject add(int index, Object item);
-
-	/**
-	 * Adds an element at the end of the list.
-	 * 
-	 * If a null object is provided to insert in the list, the
-	 * request must be ignored and an appropriate error must be
-	 * returned.
-	 * 
-	 * @param item the value to insert into the list
-	 * @return an ReturnObject, empty if the operation is successful
-	 *         the item added or containing an appropriate error message
-	 */
-	public ReturnObject add(Object item) {
-		ReturnObjectImpl newItem = new ReturnObjectImpl(item);
-		
-		if(lastElement == STORAGE) {
-			expendArray();
-			arrayList[lastElement] = newItem;
-			lastElement++;
-			return new ReturnObjectImpl();
-
+	public ReturnObject remove(int index) {
+		if(index < 0 || lastElement < index) {
+			return errObj.createErrorObject(INDEX_OUT_OF_BOUNDS);
 		}else{
+			ReturnObjectImpl result = arrayList[index];
+			if(index != lastElement) {
+				for(int i = index; i <= lastElement; i++) {
+					arrayList[i] = arrayList[i + 1]; 
+				}
+			}
+			lastElement--;
+			if(lastElement + 2 < Storage/2) {	//The + 2 is a buffer
+				collapseArray();
+			}
+			return result;
+		}
+	}
+	
+	public ReturnObject add(int index, Object item) {
+		if(index < 0 || lastElement + 1 < index) {
+			return errObj.createErrorObject(INDEX_OUT_OF_BOUNDS);
+		}else if(item == null){
+			return errObj.createErrorObject(INVALID_ARGUMENT);
+		}else{
+			ReturnObjectImpl newItem = new ReturnObjectImpl(item);
+			
+			if(lastElement + 1 == Storage) {
+				expandArray();
+			}
+			if(index == lastElement + 1) {
+				arrayList[index] = newItem;
+			}else{
+				for(int i = lastElement; index <= i ; i--) {
+					arrayList[i + 1] = arrayList[i];
+				}
+				arrayList[index] = newItem;
+			}
+			if(!isEmpty()) {
+				lastElement++;
+			}
+			return new ReturnObjectImpl();
+		}
+	}
+
+	public ReturnObject add(Object item) {
+		if(item == null){
+			return errObj.createErrorObject(INVALID_ARGUMENT);
+		}else{
+			ReturnObjectImpl newItem = new ReturnObjectImpl(item);
+			
+			if(lastElement + 1 == Storage) {
+				expandArray();
+			}
+			if(!isEmpty()) {
+				lastElement++;
+			}
 			arrayList[lastElement] = newItem;
-			lastElement++;
+			
 			return new ReturnObjectImpl();
 		}
 	}
 	
-	public void expendArray() {
-		ReturnObjectImpl[] tempArray = new ReturnObjectImpl[STORAGE * 2];
-		for(int i = 0; i < STORAGE; i++) {
+	public void expandArray() {
+		ReturnObjectImpl[] tempArray = new ReturnObjectImpl[Storage * 2];
+		for(int i = 0; i < Storage; i++) {
 			tempArray[i] = arrayList[i];
 		}
 		arrayList = tempArray;
-		STORAGE = STORAGE * 2;
+		Storage = Storage * 2;
+	}
+	
+	public void collapseArray() {
+		ReturnObjectImpl[] tempArray = new ReturnObjectImpl[Storage / 2];
+		for(int i = 0; i < Storage/2; i++) {
+			tempArray[i] = arrayList[i];
+		}
+		arrayList = tempArray;
+		Storage = Storage / 2;		
 	}
 	
 	public String toString() {
@@ -121,7 +122,7 @@ public class ArrayList implements List{
 		if(isEmpty()){
 			result = result + errObj.createErrorObject(EMPTY_STRUCTURE).getReturnValue();
 		}else{
-			for(int i = 0; i < lastElement; i++) {
+			for(int i = 0; i <= lastElement; i++) {
 				result = result + arrayList[i].getReturnValue() + " ";
 			}
 		}
