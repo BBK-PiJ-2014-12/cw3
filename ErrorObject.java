89b2716352ecb0ea465986782ebf8847db5076ce
BBK-PiJ-2014-12 <zsolt.balvanyos@gmail.com>
2015-02-01 17:43:17 +0000

    Delete ErrorObject.java
    Class not used anymore.

--- ErrorObject.java
+++
@@ -1,9 +0,0 @@
-
-public class ErrorObject {
-	public ReturnObject createErrorObject(int errorNumber) {
-		ReturnObjectImpl errMes = new ReturnObjectImpl("");
-		errMes.setErrorNum(errorNumber);
-		errMes.setItem(errMes.getError());
-		return errMes;	
-	}
-}
